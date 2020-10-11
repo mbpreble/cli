d518f23296feae58d010a041cd4405de3cf590a3
Matthew Preble <mpreble@indeed.com>
2020-10-10 23:48:06 -0500

    variable usage.

--- script/sign-windows-executable.sh
+++ script/sign-windows-executable.sh
@@ -9,9 +9,9 @@ CERTIFICATE_PATH="testCertificate.pfx"
 PROGRAM_NAME="GitHub CLI"
 
 # Probably need a password for one of these...
-openssl pkcs12 -in ${"CERTIFICATE_PATH"} -nocerts -nodes -out key.pem
+openssl pkcs12 -in ${CERTIFICATE_PATH} -nocerts -nodes -out key.pem
 openssl rsa -in key.pem -outform PVK -pvk-strong -out pvk.pvk
-openssl pkcs12 -in ${"CERTIFICATE_PATH"} -nokeys -nodes -out cert.pem
+openssl pkcs12 -in ${CERTIFICATE_PATH} -nokeys -nodes -out cert.pem
 openssl crl2pkcs7 -nocrl -certfile cert.pem -outform DER -out spc.spc
 
 signcode \
