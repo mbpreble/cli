a8dda5cc4fcbd28c59bdd1599db92588c2e105d1
Matthew Preble <mpreble@indeed.com>
2020-10-11 10:35:08 -0500

    download cert from repo intead.

--- script/sign-windows-executable.sh
+++ script/sign-windows-executable.sh
@@ -3,13 +3,16 @@ set -e
 
 EXECUTABLE_PATH=$1
 
-# Assume we... did something to get the cert downloaded
-CERTIFICATE_PATH="testCertificate.pfx"
+curl \
+  -H "Authorization: token $DESKTOP_CERT_TOKEN" \
+  -H "Accept: application/vnd.github.v3.raw" \
+  --output windows-certificate.pfx
+ https://api.github.com/repos/mbpreble/cli/contents/testCertificate.pfx # TODO - this path needs to change
 
 PROGRAM_NAME="GitHub CLI"
 
 # Convert private key to the expected format
-openssl pkcs12 -in ${CERTIFICATE_PATH} -nocerts -nodes -out private-key.pem  -passin pass:${GITHUB_CERT_PASSWORD}
+openssl pkcs12 -in windows-certificate.pfx -nocerts -nodes -out private-key.pem  -passin pass:${GITHUB_CERT_PASSWORD}
 openssl rsa -in private-key.pem -outform PVK -pvk-none -out private-key.pvk 2>/dev/null # Always writes to STDERR
 
 # Convert certificate chain into the expected format
