44595ba9bb3a4d9873370061fdc48dcaee407e38
Matthew Preble <mpreble@indeed.com>
2020-10-11 00:21:47 -0500

    iterating....

--- script/sign-windows-executable.sh
+++ script/sign-windows-executable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-PATH=$1
+EXECUTABLE_PATH=$1
 
 # Assume we... did something to get the cert downloaded
 CERTIFICATE_PATH="testCertificate.pfx"
@@ -9,7 +9,7 @@ CERTIFICATE_PATH="testCertificate.pfx"
 PROGRAM_NAME="GitHub CLI"
 
 # Probably need a password for one of these...
-openssl pkcs12 -in ${CERTIFICATE_PATH} -nocerts -nodes -out key.pem
+openssl pkcs12 -in ${CERTIFICATE_PATH} -nocerts -nodes -out key.pem  --password ${GITHUB_CERT_PASSWORD}
 openssl rsa -in key.pem -outform PVK -pvk-strong -out pvk.pvk
 openssl pkcs12 -in ${CERTIFICATE_PATH} -nokeys -nodes -out cert.pem
 openssl crl2pkcs7 -nocrl -certfile cert.pem -outform DER -out spc.spc
